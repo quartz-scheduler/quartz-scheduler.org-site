ca45eebc97e185a7645409292adfc87fcb6af243
Zemian Deng <zemian.deng@softwareag.com>
2019-02-27 12:33:19 -0500

    build: Disable mvn package since CI does not have that tool installed

--- get-deps.sh
+++ get-deps.sh
@@ -15,19 +15,19 @@ pushd __reference
 VER=2.4.0-SNAPSHOT
 git checkout master
 cp -rf docs ../documentation/${VER}
-mvn package -Pdist -DskipTests
-cp -r distribution/target/quartz-${VER}-distribution.tar.gz ../downloads/files
-mkdir -p ../api/${VER}
-cp -r distribution/target/quartz-${VER}/javadoc/* ../api/${VER}
+#mvn package -Pdist -DskipTests
+#cp -r distribution/target/quartz-${VER}-distribution.tar.gz ../downloads/files
+#mkdir -p ../api/${VER}
+#cp -r distribution/target/quartz-${VER}/javadoc/* ../api/${VER}
 
 # Copy 2.3.x version of quartz docs to a subdir:
 VER=2.3.1-SNAPSHOT
 git checkout quartz-2.3.x
 cp -rf docs ../documentation/${VER}
-mvn package -Pdist -DskipTests
-cp -r distribution/target/quartz-${VER}-distribution.tar.gz ../downloads/files
-mkdir -p ../api/${VER}
-cp -r distribution/target/quartz-${VER}/javadoc/* ../api/${VER}
+#mvn package -Pdist -DskipTests
+#cp -r distribution/target/quartz-${VER}-distribution.tar.gz ../downloads/files
+#mkdir -p ../api/${VER}
+#cp -r distribution/target/quartz-${VER}/javadoc/* ../api/${VER}
 
 # clean up
 popd
